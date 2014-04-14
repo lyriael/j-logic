43d177adf4c5471f3f084bbd9446e07e70bc0191
Judith Fuog <lyriael@gmx.net>
2014-04-14 22:05:24 +0200

    central pilers with help of ts

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 '''Using http://www.laurentluce.com/posts/binary-search-tree-library-in-python/ as help '''
 
+
 class Node:
 
-    parent = None;
-    left = None;
-    right = None;
+    parent = None
+    left = None
+    right = None
 
     def __init__(self, data):
         self.left = None
@@ -17,21 +18,74 @@ class Node:
             print("It's a axiom")
         elif self.is_union:
             print("It's a union")
-        elif self.is_eclamation:
+        elif self.is_bang:
             print("It's a strange \"!\"")
 
 
+class Formula:             # Node --> Formula
 
+    is_axiom = False     # is_leaf
 
-class Term:
+    def __init__(self, c, A):
+        self.proof_term = c
+        self.subformula = A
 
-    def __init__(self,c,A):
-        self.proof_constant = c;
-        self.axiom = A;
+    def is_axiom_of_cs(self, cs):
+        return cs[self.proof_constant] == self.axiom
 
     def to_String(self):
-        return self.proof_constant +":"+ self.axiom
+        return self.proof_constant + ":" + self.axiom
+
+    def is_provable(self, cs):
+        if self.proof_term.is_constant();
+           return self.is_axiom_of_cs(self, cs)
+        elif self.proof_term.is_sum();
+            left = Formula(self.proof_term.get_left(), self.subformula)
+            right = Formula(self.proof_term.get_right(), self.subformula)
+            return left.is_provable(cs) or right.is_provable(cs)
+        elif self.proof_term.is_bang()
+            #todo
 
 
-t=Term("(!f)", "A->B");
-print(t.to_String());
+        return True
+
+
+class Proof_Term:
+
+    def __init__(self, t):
+        self.term = t
+
+    def is_constant(self):
+        return True
+
+    def is_sum(self):
+        return True
+
+    def is_bang(self):
+        return True
+
+    def get_left(self):
+        #todo
+        return False
+
+    def get_right(self):
+        #todo
+        return False
+
+
+
+
+
+class Tree:
+
+    def __init__(self, cs, term):
+        self.root = term
+        self.leaves.push(term)
+        self.cs = cs
+
+    def grow_child(term):
+        return 42
+
+cs = {"c1": "¬A", "c2": "A->B"}
+t = Term("(!f)", "A->B")
+print(t.to_String())
