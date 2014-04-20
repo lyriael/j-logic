f9bee1c6c4b83338fd329f5ddb40ffb42f3bea4b
Judith Fuog <lyriael@gmx.net>
2014-04-20 14:59:25 +0200

    clean up of draft

--- test.py
+++ test.py
@@ -1,28 +1,10 @@
-'''Using http://www.laurentluce.com/posts/binary-search-tree-library-in-python/ as help '''
+# Formula consists of a proof term and a subformula. The subformula can also contain
+# a proof term, but eventually the subformula should derived from the Axioms.
+# As for now, the allowed axioms are: -> (implication), ¬ (not)
+# the allowed proof terms are: + (sum/union), ! (bang)
 
 
-class Node:
-
-    parent = None
-    left = None
-    right = None
-
-    def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-        print("A node has been made.")
-
-    def children(self):
-        if self.is_axiom:
-            print("It's a axiom")
-        elif self.is_union:
-            print("It's a union")
-        elif self.is_bang:
-            print("It's a strange \"!\"")
-
-
-class Formula:             # Node --> Formula
+class Formula:
 
     is_axiom = False     # is_leaf
 
@@ -30,38 +12,45 @@ class Formula:             # Node --> Formula
         self.proof_term = c
         self.subformula = A
 
-    def is_axiom_of_cs(self, cs):
-        return cs[self.proof_constant] == self.axiom
-
-    def to_String(self):
-        return self.proof_constant + ":" + self.axiom
-
+    # Main method of class Formula. Evaluates if a Formula is provable.
     def is_provable(self, cs):
-        if self.proof_term.is_constant();
-           return self.is_axiom_of_cs(self, cs)
-        elif self.proof_term.is_sum();
+        # e.g. t: A
+        if self.proof_term.is_constant():
+            return self.is_axiom_of_cs(cs)
+        # e.g. (t+s): A
+        elif self.proof_term.is_sum():
             left = Formula(self.proof_term.get_left(), self.subformula)
             right = Formula(self.proof_term.get_right(), self.subformula)
             return left.is_provable(cs) or right.is_provable(cs)
-        elif self.proof_term.is_bang()
-            #todo
+        # e.g. !t:(t: A)
+        elif self.proof_term.is_bang():
+            a = Reader.split(self.subformula)
+            new_formula = Formula(a[0], a[1])
+            if self.proof_term.remove_bang() == new_formula.proof_term:
+                return new_formula.is_provable(cs)
+            else:
+                return False
+        return False
+
+    def is_axiom_of_cs(self, cs):
+        return cs[self.proof_term] == self.subformula
 
 
-        return True
-
-
-class Proof_Term:
+class ProofTerm:
 
     def __init__(self, t):
         self.term = t
 
     def is_constant(self):
+        #todo
         return True
 
     def is_sum(self):
+        #todo
         return True
 
     def is_bang(self):
+        #todo
         return True
 
     def get_left(self):
@@ -72,20 +61,24 @@ class Proof_Term:
         #todo
         return False
 
+    def remove_bang(self):
+        #todo
+        return self
+
+## Helper class to parse String to
 
 
+class Reader:
 
+    formulas = ["atomic", "implication", "negation"]
+    proof_terms = ["atomic", "sum", "bang"]
 
-class Tree:
+    @staticmethod
+    def split(formula):
+        #todo
+        return formula
 
-    def __init__(self, cs, term):
-        self.root = term
-        self.leaves.push(term)
-        self.cs = cs
-
-    def grow_child(term):
-        return 42
 
 cs = {"c1": "¬A", "c2": "A->B"}
-t = Term("(!f)", "A->B")
+t = Formula("(!f)", "A->B")
 print(t.to_String())
