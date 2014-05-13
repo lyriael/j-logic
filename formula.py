359d36990d7692bd1d2d4489ea8385262a8a2a2d
lyriael <lyriael@gmx.net>
2014-05-13 19:55:46 +0200

    nicer output for tests

--- formula.py
+++ formula.py
@@ -15,32 +15,42 @@ class Formula(object):
     def __str__(self):
         return str(self._tree)
 
-    def is_provable(self, cs):
+    def is_provable(self, cs, indent=''):
         proof_term = self.proof_term()
         subformula = self.subformula()
-        print('-------------------------')
-        print('is provable? ' + str(self))
-        print('--> Proof term: ' + str(proof_term) + ',\t token: ' + proof_term.token())
-        print('--> Subformula: ' + str(subformula) + ',\t token: ' + subformula.token())
+        print(indent + ' is ' + str(self) + ' provable?')
+        print(indent + ' \tProof term: ' + str(proof_term) + ',\t token: ' + proof_term.token())
+        print(indent + ' \tSubformula: ' + str(subformula) + ',\t token: ' + subformula.token())
 
         if proof_term.is_leaf():  # if_constant
-            print('--> --> is leaf')
+            print(indent + ' operation type is "constant"')
+            print(indent + ' \t"'+str(proof_term)+' => ' + str(subformula) + '" in cs? ')
+            print(indent + ' \t' + str(self.is_axiom(cs)) + '!')
+            print('')
             return self.is_axiom(cs)
         elif proof_term.token() == '!':  # if_bang
-            print('--> --> is bang')
-            print('--> --> --> ' + str(subformula))
+            print(indent + ' operation type is "!"')
+            print(indent + ' \tProof term and subformula have same constant?')
             new_formula = Formula(str(subformula))
+
             if proof_term.inorder()[1:] == new_formula.proof_term().inorder():
-                return new_formula.is_provable(cs)
+                print(indent + ' \tTrue!')
+                indent += ' -->'
+                print('')
+                return new_formula.is_provable(cs, indent)
             else:
+                print(indent + ' \tFalse!')
+                print('')
                 return False
         elif proof_term.token() == '+':  # if_sum
-            print('--> --> is sum')
-            print('--> --> --> left: (' + str(proof_term.left()) + ":" + str(subformula) + ')')
-            print('--> --> --> right: (' + str(proof_term.right()) + ":" + str(subformula) + ')')
+            print(indent + ' operation type is "+"')
+            print(indent + ' \tleft: (' + str(proof_term.left()) + ":" + str(subformula) + ')')
+            print(indent + ' \tright: (' + str(proof_term.right()) + ":" + str(subformula) + ')')
+            print('')
+            indent += ' -->'
             left_formula = Formula('(' + str(proof_term.left()) + ":" + str(subformula) + ')')
             right_formula = Formula('(' + str(proof_term.right()) + ":" + str(subformula) + ')')
-            return left_formula.is_provable(cs) or right_formula.is_provable(cs)
+            return left_formula.is_provable(cs, indent) or right_formula.is_provable(cs, indent)
 
     def proof_term(self):
         return self._tree.left().subtree()
@@ -49,12 +59,6 @@ class Formula(object):
         return self._tree.right().subtree()
 
     def is_axiom(self, cs):
-        #debug stuff
-        print('----is axiom?----')
-        print('--> proof term: ' + self.proof_term().token())
-        print('--> subformula: ' + self.subformula().token())
-        print('--> cs: ' + str(cs))
-        print('--> --> ' + str(cs[str(self.proof_term())] == str(self.subformula())))
         proof_term = self.proof_term().token()
         subformula = self.subformula().token()
         return cs[proof_term] == subformula
