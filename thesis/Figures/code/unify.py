8bf375b44740dbe88e70ed849447eea47e06f210
Judith Fuog <lyriael@gmx.net>
2015-03-31 10:46:09 +0200

    final review DONE

--- thesis/Figures/code/unify.py
+++ thesis/Figures/code/unify.py
@@ -4,7 +4,7 @@ while stack:
   f1, f2 = stack.pop()
   # If the root node is the same (either operation or constant)
   if f1.root.token == f2.root.token:
-    # If the its a operator, go on. If it's a constant we're done.
+    # If its a operator, go on. If it's a constant we're done.
     if f1.root.token in ['->', ':']:
       stack.append(
       	(subtree(f1.root.left), subtree(f2.root.left)))
